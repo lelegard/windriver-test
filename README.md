17f2f65e0a1ac08af668fdd0ac0db7968553c7ca
Thierry Lelegard <thierry@lelegard.fr>
2023-03-18 23:54:59 +0100

    Removed pre-wdm files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ---
 page_type: sample
-description: "Demonstrates how to write a non-PnP driver using the Kernel Mode Driver Framework."
+description: "Demonstrates usage of four different types of IOCTLs."
 languages:
 - cpp
 products:
@@ -8,65 +8,17 @@ products:
 - windows-wdk
 ---
 
-# Non-PnP Driver Sample
+# IOCTL
 
-This sample is primarily meant to demonstrate how to write a NON-PNP driver using the Kernel Mode Driver Framework.
+This sample demonstrates the usage of four different types of IOCTLs (METHOD\_IN\_DIRECT, METHOD\_OUT\_DIRECT, METHOD\_NEITHER, and METHOD\_BUFFERED).
 
-It also illustrates several other important framework interfaces. Following table gives a typical usage scenario and summarizes all the features used in this sample.
+The sample shows how the user input and output buffers specified in the **DeviceIoControl** function call are handled, in each case, by the I/O subsystem and the driver.
 
-This sample would be useful for writing a driver that does not interact with any hardware. Typically, such drivers are written to provide some kernel-level services to a user application. These drivers are dynamically loaded by the application when it is run and unloaded when it exits.
-
-(Examples: FileMon, Regmon, DeviceTree are examples of tools that use this type of driver.)
-
-- How to Write a NON PNP driver
-
-- How to register EvtPreProcessCallback to handle requests in the context of the calling thread
-
-- Show how to probe and lock buffers in the preprocess callback for METHOD\_NEITHER IOCTL requests
-
-- Also show how to handle other 3 types of IOCTLs (METHOD\_BUFFERED, METHOD\_IN\_DIRECT & METHOD\_OUT\_DIRECT)
-
-- How to open a file in Kernel-mode and Read & Write to it
-
-- Finally show event tracing and dumping variable length data in the tracelog using HEXDUMP format.
-
-The sample is accompanied by a simple multithreaded Win32 console application to test the driver.
+The sample consists of a legacy device driver and a Win32 console test application. The test application opens a handle to the device exposed by the driver and makes all four different **DeviceIoControl** calls, one after another. To understand how the IRP fields are set the I/O manager, you should run the checked build version of the driver and look at the debug output.
 
 > [!CAUTION]
-> This is a minimal driver meant to demonstrate an OS feature. Neither it nor its sample programs are intended for use in a production environment. Rather, they are intended for educational purposes and as a skeleton driver.
+> This sample driver is not a Plug and Play driver. This is a minimal driver meant to demonstrate a feature of the operating system. Neither this driver nor its sample programs are intended for use in a production environment. Instead, they are intended for educational purposes and as a skeleton driver.
 
-## Build the sample
+## Run the sample
 
-For information on how to build a driver solution using Microsoft Visual Studio, see [Building a Driver with Visual Studio and the WDK](https://docs.microsoft.com/windows-hardware/drivers/develop/building-a-driver).
-
-If the build succeeds, you will find the driver, nonpnp.sys, and the test application, nonpnpapp.exe, in the binary output directory specified for the build environment.
-
-To test this driver, copy the nonpnp.inf into the same folder as the nonpnpapp.exe and the wdfcoinstaller\<version\>.dll .
-
-> [!NOTE]
-> You can obtain redistributable framework updates by downloading the *wdfcoinstaller.msi* package from [WDK 8 Redistributable Components](https://go.microsoft.com/fwlink/p/?LinkID=253170). This package performs a silent install into the directory of your Windows Driver Kit (WDK) installation. You will see no confirmation that the installation has completed. You can verify that the redistributables have been installed on top of the WDK by ensuring there is a redist\\wdf directory under the root directory of the WDK, %ProgramFiles(x86)%\\Windows Kits\\8.0.
-
-Next, run nonpnpapp.exe, a simple Win32 multithreaded console mode application. The driver will be automatically loaded and started. When you exit the app, the driver will be stopped and removed.
-
-Usage: nonpnpapp.exe (-l) (-v version)
-
-> [!NOTE]
-> This application first tries to open the device (\\Device\\FileIo). If the device doesn't exist, it takes that as a hint that the driver is not loaded and tries to load the driver using service control manager API. If the service is loaded successfully, it tries to open the device again. If successful, it makes all four different types of DeviceControl calls to the driver. After that it makes a WriteFile call with an arbitrary size buffer. The driver, in response, writes that buffer to a file opened in the Create request. The name of the file was provided by the application as part of the device name and the directory path is hardcoded to %WINDIR%\\temp. When the WriteFile returns, the application makes a ReadFile call to read the file through the driver, and then compares the data returned by the driver with the one it originally wrote. If you specify -l option in command line, the application does this Write and Read operation in an infinite loop. The -v command line option is used to specify the version of the KMDF coinstaller (wdfcoinstaller\<version\>.dll) to load. If none is specified then it loads the coinstaller for v1.0 (wdfcoinstaller01000.dll)
-
-### WDF SECTION
-
-Nonpnp drivers typically don't need an INF file to install. Since we are using framework interfaces, we have to use the Kmdf coinstaller to install the framework binaries on the target machine. The Kmdf coinstaller needs a WDF specific section in the INF to get the driver service name and the version of the Kmdf library the driver is bound to. The syntax and description of the section is given below. Any non inf based driver using Kmdf library will need to have a dummy inf file with the wdf section in it. The format of the Wdf section is given below:
-
-[Version]
-
-Signature="\$WINDOWS NT\$"
-
-[\<driver name\>.NT.Wdf]
-
-KmdfService = \<your driver service name here\>, \<driver service install subsection\>
-
-[\<driver service install subsection\>]
-
-KmdfLibraryVersion = \<version bound to here, in Major.minor format\>
-
-For example, for V1.0 KmdfLibraryVersion is "1.0"
+To test this driver, copy the test app, Ioctlapp.exe, and the driver to the same directory, and run the application. The application will automatically load the driver, if it's not already loaded, and interact with the driver. When you exit the application, the driver will be stopped, unloaded and removed.
--- exe/testapp.cpp
+++ exe/testapp.cpp
@@ -1,17 +1,10 @@
-#include <DriverSpecs.h>
 #include <windows.h>
-
-#pragma warning(disable:4201)  // nameless struct/union
 #include <winioctl.h>
-#pragma warning(default:4201)
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <limits.h>
+#include <string.h>
 #include <strsafe.h>
-#include <cstring>
-#include "public.h"
+#include <sys\sioctl.h>
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -20,6 +13,11 @@
 #if defined(min)
 #undef min
 #endif
+#if defined(max)
+#undef max
+#endif
+
+#pragma warning(disable: 4100) // unreferenced formal parameter
 
 std::string Format(const char* fmt, ...)
 {
@@ -53,7 +51,7 @@ std::string ErrorMessage(::DWORD code = ::GetLastError())
     return message.empty() ? Format("System error %d (0x%X)", int(code), int(code)) : message;
 }
 
-bool InstallDriverFile(std::string& driver_path)
+bool GetDriverFile(std::string& driver_path)
 {
     // Driver file name.
     const std::string driver_basename(DRIVER_NAME ".sys");
@@ -71,251 +69,291 @@ bool InstallDriverFile(std::string& driver_path)
     std::cout << "App dir: " << file << std::endl;
 
     // Check if driver is present in same directory as application.
-    std::string driver_file(file + '\\' + driver_basename);
-    if (::GetFileAttributesA(driver_file.c_str()) == INVALID_FILE_ATTRIBUTES) {
+    driver_path = file + '\\' + driver_basename;
+    if (::GetFileAttributesA(driver_path.c_str()) == INVALID_FILE_ATTRIBUTES) {
         // Not found, replace '/exe/' with '/sys/' in the directory.
-        pos = driver_file.rfind("\\exe\\");
+        pos = driver_path.rfind("\\exe\\");
         if (pos != std::string::npos) {
-            driver_file.replace(pos + 1, 3, "sys");
+            driver_path.replace(pos + 1, 3, "sys");
         }
     }
 
     // Check if driver file exists.
-    if (::GetFileAttributesA(driver_file.c_str()) == INVALID_FILE_ATTRIBUTES) {
-        std::cout << "Driver file not found: " << driver_file << std::endl;
+    if (::GetFileAttributesA(driver_path.c_str()) == INVALID_FILE_ATTRIBUTES) {
+        std::cout << "Driver file not found: " << driver_path << std::endl;
         return false;
     }
-    std::cout << "Driver: " << driver_file << std::endl;
+    std::cout << "Driver: " << driver_path << std::endl;
+    return true;
+}
 
-    // Get Windows directory (typically C:\Windows)
-    driver_path.resize(MAX_PATH);
-    length = ::GetWindowsDirectoryA(&driver_path[0], MAX_PATH);
-    driver_path.resize(std::min<size_t>(length, MAX_PATH));
-
-    // Build final driver location.
-    driver_path += "\\System32\\Drivers\\";
-    driver_path += driver_basename;
-
-    // Install the driver in system area.
-    if (!::CopyFileA(driver_file.c_str(), driver_path.c_str(), false)) {
-        const ::DWORD err = ::GetLastError();
-        std::cout << "CopyFile() error: " << ErrorMessage(err) << ", destination: " << driver_path << std::endl;
-        return false;
-    }
-
+bool InitDriver(const std::string& DriverName, const std::string& DriverExe)
+{
     // Connect to the Service Control Manager and open the Services database.
-    const ::SC_HANDLE schSCManager = ::OpenSCManagerA(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-    if (!schSCManager) {
+    const ::SC_HANDLE schSCManager = ::OpenSCManagerA(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
+    if (schSCManager == nullptr) {
         const ::DWORD err = ::GetLastError();
         std::cout << "OpenSCManagerA() error: " << ErrorMessage(err) << std::endl;
         return false;
     }
 
-    // Create a new a service object.
-    std::cout << "CreateService(\"" << DRIVER_NAME << "\", \"" << driver_path << "\")" << std::endl;
-    const ::SC_HANDLE schService = ::CreateServiceA(schSCManager,
-        DRIVER_NAME,            // address of name of service to start
-        DRIVER_NAME,            // address of display name
+    // Create a new service object.
+    ::SC_HANDLE schService = ::CreateServiceA(schSCManager,           // handle of service control manager database
+        DriverName.c_str(),             // address of name of service to start
+        DriverName.c_str(),             // address of display name
         SERVICE_ALL_ACCESS,     // type of access to service
         SERVICE_KERNEL_DRIVER,  // type of service
         SERVICE_DEMAND_START,   // when to start service
         SERVICE_ERROR_NORMAL,   // severity if service fails to start
-        driver_path.c_str(),    // address of name of binary file
-        nullptr,                // service does not belong to a group
-        nullptr,                // no tag requested
-        nullptr,                // no dependency names
-        nullptr,                // use LocalSystem account
-        nullptr);               // no password for service account
-
+        DriverExe.c_str(),             // address of name of binary file
+        NULL,                   // service does not belong to a group
+        NULL,                   // no tag requested
+        NULL,                   // no dependency names
+        NULL,                   // use LocalSystem account
+        NULL                    // no password for service account
+    );
     bool ok = schService != nullptr;
     if (!ok) {
-        const ::DWORD err = ::GetLastError();
+        ::DWORD err = ::GetLastError();
         if (err == ERROR_SERVICE_EXISTS) {
+            // Ignore this error.
+            std::cout << "Service " << DriverName << " already exists" << std::endl;
+            // Open existing service
+            schService = ::OpenServiceA(schSCManager, DriverName.c_str(), SERVICE_ALL_ACCESS);
+            ok = schService != nullptr;
+            err = ::GetLastError();
+        }
+        if (!ok) {
+            std::cout << "CreateService()/OpenService() error: " << ErrorMessage(err) << std::endl;
+            CloseServiceHandle(schSCManager);
+            return false;
+        }
+    }
+
+    // Start the execution of the service (i.e. start the driver).
+    ok = ::StartServiceA(schService, 0, nullptr);
+    if (!ok) {
+        const ::DWORD err = ::GetLastError();
+        if (err == ERROR_SERVICE_ALREADY_RUNNING) {
+            // Ignore this error.
+            std::cout << "Service " << DriverName << " already started" << std::endl;
             ok = true;
         }
         else {
-            std::cout << "CreateService() error: " << ErrorMessage(err) << std::endl;
+            std::cout << "StartServiceA() error: " << ErrorMessage(err) << std::endl;
         }
     }
-    else {
-        // Start the execution of the service (i.e. start the driver).
-        ok = ::StartServiceA(schService, 0, NULL);
-        if (!ok) {
-            const ::DWORD err = ::GetLastError();
-            if (err == ERROR_SERVICE_ALREADY_RUNNING) {
-                ok = true;
-            }
-            else {
-                std::cout << "StartServiceA() error: " << ErrorMessage(err) << std::endl;
-            }
-        }
-        // Close the service object.
-        CloseServiceHandle(schService);
-    }
+
+    // Close the service object.
+    ::CloseServiceHandle(schService);
 
     // Close handle to service control manager.
     ::CloseServiceHandle(schSCManager);
+
     return ok;
 }
 
-bool RemoveDriverFile(const std::string& driver_path)
+bool TerminateDriver(const std::string& DriverName)
 {
-    printf("RemoveDriverFile(\"%s\")\n", driver_path.c_str());
-
     // Connect to the Service Control Manager and open the Services database.
-    const ::SC_HANDLE schSCManager = ::OpenSCManagerA(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-    if (!schSCManager) {
+    const ::SC_HANDLE schSCManager = ::OpenSCManagerA(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
+    if (schSCManager == nullptr) {
         const ::DWORD err = ::GetLastError();
         std::cout << "OpenSCManagerA() error: " << ErrorMessage(err) << std::endl;
         return false;
     }
 
     // Open the handle to the existing service.
-    const ::SC_HANDLE schService = ::OpenServiceA(schSCManager, DRIVER_NAME, SERVICE_ALL_ACCESS);
-    bool ok = schService != nullptr;
+    ::SC_HANDLE schService = ::OpenServiceA(schSCManager, DriverName.c_str(), SERVICE_ALL_ACCESS);
+    if (schService == nullptr) {
+        const ::DWORD err = ::GetLastError();
+        std::cout << "OpenServiceA() error: " << ErrorMessage(err) << std::endl;
+        ::CloseServiceHandle(schSCManager);
+        return false;
+    }
+
+    // Request that the service stop.
+    ::SERVICE_STATUS serviceStatus;
+    bool ok = ::ControlService(schService, SERVICE_CONTROL_STOP, &serviceStatus);
     if (!ok) {
-        printf("OpenServiceA() error: %d \n", ::GetLastError());
+        const ::DWORD err = ::GetLastError();
+        std::cout << "ControlService(STOP) error: " << ErrorMessage(err) << std::endl;
     }
-    else {
-        // Request that the service stop.
-        ::SERVICE_STATUS serviceStatus;
-        ok = ::ControlService(schService, SERVICE_CONTROL_STOP, &serviceStatus);
-        if (!ok) {
-            const ::DWORD err = ::GetLastError();
-            std::cout << "ControlService() error: " << ErrorMessage(err) << std::endl;
-        }
-        else {
-            // Mark the service for deletion from the service control manager database.
-            ok = ::DeleteService(schService);
-            if (!ok) {
-                const ::DWORD err = ::GetLastError();
-                std::cout << "DeleteService() error: " << ErrorMessage(err) << std::endl;
-            }
-        }
-        // Close the service object.
-        CloseServiceHandle(schService);
+
+    // Mark the service for deletion from the service control manager database.
+    if (!::DeleteService(schService)) {
+        const ::DWORD err = ::GetLastError();
+        std::cout << "DeleteService() error: " << ErrorMessage(err) << std::endl;
+        ok = false;
     }
+
+    // Close the service object.
+    ::CloseServiceHandle(schService);
 
     // Close handle to service control manager.
     ::CloseServiceHandle(schSCManager);
+
     return ok;
 }
 
 
-VOID DoIoctls(HANDLE hDevice)
+int main(int argc, const char* argv[])
 {
-    char OutputBuffer[100];
-    char InputBuffer[200];
+    HANDLE hDevice;
     BOOL bRc;
     ULONG bytesReturned;
+    DWORD errNum = 0;
+    std::string driverLocation;
+    char OutputBuffer[100];
+    char InputBuffer[100];
 
-    // Performing METHOD_BUFFERED
-    if (FAILED(StringCchCopy(InputBuffer, sizeof(InputBuffer), "this String is from User Application; using METHOD_BUFFERED"))) {
-        return;
+    // open the device
+    if ((hDevice = CreateFile( "\\\\.\\IoctlTest",
+                            GENERIC_READ | GENERIC_WRITE,
+                            0,
+                            NULL,
+                            CREATE_ALWAYS,
+                            FILE_ATTRIBUTE_NORMAL,
+                            NULL)) == INVALID_HANDLE_VALUE) {
+
+        errNum = GetLastError();
+
+        if (errNum != ERROR_FILE_NOT_FOUND) {
+            printf("CreateFile failed : %d\n", errNum);
+            return EXIT_FAILURE;
+        }
+
+        // The driver is not started yet so let us the install the driver.
+        // First setup full path to driver name.
+        //
+        if (!GetDriverFile(driverLocation) || !InitDriver(DRIVER_NAME, driverLocation)) {
+            return EXIT_FAILURE;
+        }
+
+        hDevice = CreateFile( "\\\\.\\IoctlTest",
+                            GENERIC_READ | GENERIC_WRITE,
+                            0,
+                            NULL,
+                            CREATE_ALWAYS,
+                            FILE_ATTRIBUTE_NORMAL,
+                            NULL);
+
+        if ( hDevice == INVALID_HANDLE_VALUE ){
+            printf ( "Error: CreatFile Failed : %d\n", GetLastError());
+            return EXIT_FAILURE;
+        }
+
     }
+
+    //
+    // Printing Input & Output buffer pointers and size
+    //
+
+    printf("InputBuffer Pointer = %p, BufLength = %Iu\n", InputBuffer,
+                        sizeof(InputBuffer));
+    printf("OutputBuffer Pointer = %p BufLength = %Iu\n", OutputBuffer,
+                                sizeof(OutputBuffer));
+    //
+    // Performing METHOD_BUFFERED
+    //
+
+    StringCbCopy(InputBuffer, sizeof(InputBuffer),
+        "This String is from User Application; using METHOD_BUFFERED");
+
     printf("\nCalling DeviceIoControl METHOD_BUFFERED:\n");
+
     memset(OutputBuffer, 0, sizeof(OutputBuffer));
-    bRc = DeviceIoControl(hDevice, (DWORD)IOCTL_NONPNP_METHOD_BUFFERED,
-        InputBuffer, (DWORD)strlen(InputBuffer) + 1,
-        OutputBuffer, sizeof(OutputBuffer),
-        &bytesReturned, NULL);
-    if (!bRc) {
-        printf("Error in DeviceIoControl : %d", GetLastError());
-        return;
+
+    bRc = DeviceIoControl ( hDevice,
+                            (DWORD) IOCTL_SIOCTL_METHOD_BUFFERED,
+                            &InputBuffer,
+                            (DWORD) strlen ( InputBuffer )+1,
+                            &OutputBuffer,
+                            sizeof( OutputBuffer),
+                            &bytesReturned,
+                            NULL
+                            );
+
+    if ( !bRc ) {
+        printf ( "Error in DeviceIoControl : %d", GetLastError());
+        return EXIT_FAILURE;
     }
     printf("    OutBuffer (%d): %s\n", bytesReturned, OutputBuffer);
 
-
     // Performing METHOD_NEITHER
     printf("\nCalling DeviceIoControl METHOD_NEITHER\n");
-    if (FAILED(StringCchCopy(InputBuffer, sizeof(InputBuffer), "this String is from User Application; using METHOD_NEITHER"))) {
-        return;
-    }
+    StringCbCopy(InputBuffer, sizeof(InputBuffer), "This String is from User Application; using METHOD_NEITHER");
     memset(OutputBuffer, 0, sizeof(OutputBuffer));
-    bRc = DeviceIoControl(hDevice, (DWORD)IOCTL_NONPNP_METHOD_NEITHER,
-        InputBuffer, (DWORD)strlen(InputBuffer) + 1,
-        OutputBuffer, sizeof(OutputBuffer),
-        &bytesReturned, NULL);
-    if (!bRc) {
-        printf("Error in DeviceIoControl : %d\n", GetLastError());
-        return;
+
+    bRc = DeviceIoControl ( hDevice,
+                            (DWORD) IOCTL_SIOCTL_METHOD_NEITHER,
+                            &InputBuffer,
+                            (DWORD) strlen ( InputBuffer )+1,
+                            &OutputBuffer,
+                            sizeof( OutputBuffer),
+                            &bytesReturned,
+                            NULL
+                            );
+
+    if ( !bRc )
+    {
+        printf ( "Error in DeviceIoControl : %d\n", GetLastError());
+        return EXIT_FAILURE;
     }
     printf("    OutBuffer (%d): %s\n", bytesReturned, OutputBuffer);
 
     // Performing METHOD_IN_DIRECT
     printf("\nCalling DeviceIoControl METHOD_IN_DIRECT\n");
-    if (FAILED(StringCchCopy(InputBuffer, sizeof(InputBuffer), "this String is from User Application; using METHOD_IN_DIRECT"))) {
-        return;
-    }
-    if (FAILED(StringCchCopy(OutputBuffer, sizeof(OutputBuffer), "This String is from User Application in OutBuffer; using METHOD_IN_DIRECT"))) {
-        return;
-    }
-    bRc = DeviceIoControl(hDevice, (DWORD)IOCTL_NONPNP_METHOD_IN_DIRECT,
-        InputBuffer, (DWORD)strlen(InputBuffer) + 1,
-        OutputBuffer, sizeof(OutputBuffer),
-        &bytesReturned, NULL);
-    if (!bRc) {
-        printf("Error in DeviceIoControl : : %d", GetLastError());
-        return;
-    }
-    printf("    Number of bytes transfered from OutBuffer: %d\n", bytesReturned);
+    StringCbCopy(InputBuffer, sizeof(InputBuffer), "This String is from User Application; using METHOD_IN_DIRECT");
+    StringCbCopy(OutputBuffer, sizeof(OutputBuffer), "This String is from User Application in OutBuffer; using METHOD_IN_DIRECT");
 
+    bRc = DeviceIoControl ( hDevice,
+                            (DWORD) IOCTL_SIOCTL_METHOD_IN_DIRECT,
+                            &InputBuffer,
+                            (DWORD) strlen ( InputBuffer )+1,
+                            &OutputBuffer,
+                            sizeof( OutputBuffer),
+                            &bytesReturned,
+                            NULL
+                            );
+
+    if ( !bRc )
+    {
+        printf ( "Error in DeviceIoControl : %d", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("    Number of bytes transfered from OutBuffer: %d\n",
+                                    bytesReturned);
+
+    //
     // Performing METHOD_OUT_DIRECT
+    //
+
     printf("\nCalling DeviceIoControl METHOD_OUT_DIRECT\n");
-    if (FAILED(StringCchCopy(InputBuffer, sizeof(InputBuffer), "this String is from User Application; using METHOD_OUT_DIRECT"))) {
-        return;
-    }
+    StringCbCopy(InputBuffer, sizeof(InputBuffer),
+               "This String is from User Application; using METHOD_OUT_DIRECT");
     memset(OutputBuffer, 0, sizeof(OutputBuffer));
-    bRc = DeviceIoControl(hDevice, (DWORD)IOCTL_NONPNP_METHOD_OUT_DIRECT,
-        InputBuffer, (DWORD)strlen(InputBuffer) + 1,
-        OutputBuffer, sizeof(OutputBuffer),
-        &bytesReturned, NULL);
-    if (!bRc) {
-        printf("Error in DeviceIoControl : : %d", GetLastError());
-        return;
+    bRc = DeviceIoControl ( hDevice,
+                            (DWORD) IOCTL_SIOCTL_METHOD_OUT_DIRECT,
+                            &InputBuffer,
+                            (DWORD) strlen ( InputBuffer )+1,
+                            &OutputBuffer,
+                            sizeof( OutputBuffer),
+                            &bytesReturned,
+                            NULL
+                            );
+
+    if ( !bRc )
+    {
+        printf ( "Error in DeviceIoControl : %d", GetLastError());
+        return EXIT_FAILURE;
     }
+
     printf("    OutBuffer (%d): %s\n", bytesReturned, OutputBuffer);
-}
-
-
-#pragma warning(disable: 4100)
-
-int main(int argc, const char* argv[])
-{
-    std::string driver_path;
-
-    // Open the device
-    ::HANDLE hDevice = ::CreateFile(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (hDevice == INVALID_HANDLE_VALUE) {
-        ::DWORD err = ::GetLastError();
-        if (err != ERROR_FILE_NOT_FOUND && err != ERROR_PATH_NOT_FOUND) {
-            std::cout << "CreateFile(\"" << DEVICE_NAME << "\") error: " << ErrorMessage(err) << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        // The driver is not started yet so let us install the driver.
-        // First setup full path to driver name.
-        if (!InstallDriverFile(driver_path)) {
-            return EXIT_FAILURE;
-        }
-
-        hDevice = CreateFile(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-        if (hDevice == INVALID_HANDLE_VALUE) {
-            err = ::GetLastError();
-            std::cout << "CreateFile(\"" << DEVICE_NAME << "\") error: " << ErrorMessage(err) << std::endl;
-            return EXIT_FAILURE;
-        }
-    }
-
-    // Run the tests.
-    DoIoctls(hDevice);
-
-    // Close the handle to the device before unloading the driver.
     CloseHandle(hDevice);
 
-    // Unload the driver, if was loaded.
-    if (!driver_path.empty()) {
-        RemoveDriverFile(driver_path);
-    }
+    // Unload the driver.  Ignore any errors.
+    TerminateDriver(DRIVER_NAME);
     return EXIT_SUCCESS;
 }
